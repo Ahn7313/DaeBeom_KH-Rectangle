8bcd87ef463775c4c0e9cee8c9ee906641db82e8
daebeom <andaebum6465@gmail.com>
2021-08-03 14:08:02 +0900

    TestCommit\

--- Rectangle.java
+++ Rectangle.java
@@ -24,33 +24,42 @@ public class Rectangle extends Shape {
 		this.width = width;
 		this.height = height;
 	}
+	
 	public Point[] getPoints() {
 		return points;
 	}
+	
 	public void setPoints(Point[] points) {
 		this.points = points;
 	}
+	
 	public int getWidth() {
 		return width;
 	}
+	
 	public void setWidth(int width) {
 		this.width = width;
 	}
+	
 	public int getHeight() {
 		return height;
 	}
+	
 	public void setHeight(int height) {
 		this.height = height;
 	}
+	
 	@Override
 	public void draw() {
 		System.out.println("네 꼭지점이 " + points[0] + ", " + points[1] + ", " + points[2] + ", " + points[3] + ", " 
 				+ "이고, 너비가 " + width + ", 높이가 " + height + "인 사각형을 그린다.");
 	}
+	
 	@Override
 	public double getArea() {
 		return width * height;
 	}
+	
 	@Override
 	public String toString() {
 		return "좌표 = [" + points[0] + ", " + points[1] + ", " + points[2] + ", " + points[3] + "], 너비 = " 
